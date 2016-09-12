9017207878ee3f2ff665241dc90d482e7495974b
Daniel Hum <dan@oneuphero.com>
2016-09-12 16:17:47 +0800

    exclude useless mons

--- app/workers/update_spawns.rb
+++ app/workers/update_spawns.rb
@@ -4,7 +4,9 @@ class UpdateSpawns
   KEY_INSERTED = 'update_spawns:inserted_meta'
 
   def perform(since: nil)
-    nums  = Pokemon.pluck(:pokedex_number)
+    nums = [
+      1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,116,117,118,119,120,121,123,124,125,126,127,129,130,131,133,134,135,136,137,138,139,140,141,142,143,147,148,149
+    ]
     nums -= [
       144, # Articuno
       145, # Zapdos
@@ -21,7 +23,8 @@ class UpdateSpawns
     shuffled = nums.shuffle
     idx = 65+rand(10)
     slices = shuffled[0,idx], shuffled[idx..-1]
-    slices do |mons|
+    slices.each_with_index do |mons,i|
+      mons = mons.sort
       url = ENV['UPDATE_SPAWNS_ENDPOINT'] + "?since=#{since}&mons=#{mons.join(',')}"
       domain = ENV['UPDATE_SPAWNS_DOMAIN']
       puts url
@@ -43,7 +46,7 @@ class UpdateSpawns
         }
       end
 
-      if meta = json['meta']
+      if i == 0 and meta = json['meta']
         inserted = meta['inserted']
         puts "inserted: #{inserted}"
         $redis.set KEY_INSERTED, inserted if !inserted.blank?
